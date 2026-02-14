96e1f392d03e51793135a190d27dc7f51f63f16b
AlexGarcio <redalex24@gmail.com>
2026-02-14 16:44:28 -0500

    Refactor framework detection logic to improve clarity and ensure proper polling for available frameworks

--- server/framework.lua
+++ server/framework.lua
@@ -2,30 +2,51 @@ FrameworkName = nil
 ESX = nil
 QBCore = nil
 
-if not GetResourceState('es_extended') == 'missing' then
-    FrameworkName = 'es_extended'
-    ESX = exports['es_extended']:getSharedObject()
-
-    RegisterNetEvent('esx:playerLoaded', function(playerId)
-        playerLoaded(playerId)
-    end)
-
-elseif not GetResourceState('qbx_core') == 'missing' then
-    FrameworkName = 'qbx_core'
-    QBCore = exports['qb-core']:GetCoreObject()
-
-    RegisterNetEvent('QBCore:Server:PlayerLoaded', function(player)
-        playerLoaded(player.PlayerData.source)
-    end)
-
-elseif not GetResourceState('qbox_core') == 'missing' then
-    FrameworkName = 'qbox_core'
-    QBCore = exports['qbox_core']:GetCoreObject()
-
-    RegisterNetEvent('QBCore:Server:PlayerLoaded', function(player)
-        playerLoaded(player.PlayerData.source)
-    end)
-
-else
+-- If none of the frameworks are present at this moment, show a warning and continue polling
+if GetResourceState('es_extended') == 'missing' and GetResourceState('qbx_core') == 'missing' and GetResourceState('qbox_core') == 'missing' then 
+    print(Text.frameworkerror)
     return
 end
+
+-- Poll every second until a framework is started and detected
+CreateThread(function()
+    local startTime = os.time()
+    local timeout = 300 -- seconds (5 minutes)
+    while FrameworkName == nil do
+        if os.time() - startTime >= timeout then
+            print("^1[FiveM-BanSql] No framework detected within 5 minutes, disabling resource^7")
+            StopResource(GetCurrentResourceName())
+            return
+        end
+        if GetResourceState('es_extended') == 'started' then
+            FrameworkName = 'es_extended'
+            ESX = exports['es_extended']:getSharedObject()
+            RegisterNetEvent('esx:playerLoaded', function(playerId)
+                playerLoaded(playerId)
+            end)
+            print('FiveM-BanSql: using es_extended')
+            break
+
+        elseif GetResourceState('qbx_core') == 'started' then
+            FrameworkName = 'qbx_core'
+            QBCore = exports['qb-core']:GetCoreObject()
+            RegisterNetEvent('QBCore:Server:PlayerLoaded', function(player)
+                playerLoaded(player.PlayerData.source)
+            end)
+            print('FiveM-BanSql: using qbx_core')
+            break
+
+        elseif GetResourceState('qbox_core') == 'started' then
+            FrameworkName = 'qbox_core'
+            QBCore = exports['qbox_core']:GetCoreObject()
+            RegisterNetEvent('QBCore:Server:PlayerLoaded', function(player)
+                playerLoaded(player.PlayerData.source)
+            end)
+            print('FiveM-BanSql: using qbox_core')
+            break
+        end
+
+        Wait(1000)
+    end
+end)
+
